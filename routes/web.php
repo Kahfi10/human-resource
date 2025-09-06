52d37165ae796460e344721f02d5b284ae78c211
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-09-06 13:34:45 +0800

    Implement models and views for Companies, Contracts, Departments, Designations, Employees, Payments, and Payrolls; add relationships and scopes in Payroll and Salary models.

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
